d4189ef9c5817e41a593966c471852b5212cc227
YoonYeoSun <duaeoddl01@gmail.com>
2026-07-11 11:26:36 +0900

    쇼핑몰 프로젝트 시작

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <section className="app">
+        <h2>쇼핑몰 라우팅</h2>
+      </section>
+    </>
+  )
+}
+
+export default App
